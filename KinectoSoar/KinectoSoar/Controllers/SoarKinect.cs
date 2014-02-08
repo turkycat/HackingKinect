61d78368bb9c67d19c3892b221c8aa9861163f64
Jesse Frush <jesse.frush@gmail.com>
2014-02-08 02:59:28 -0600

    kinect controls added
    - added SoarKinect.cs
    - built basic bird control logic using Kinect

---
+++ KinectoSoar/KinectoSoar/Controllers/SoarKinect.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace KinectoSoar.Controllers
+{
+    using Microsoft.Kinect;
+    using System.IO;
+
+    public class SoarKinect : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        private KinectSensor _sensor;
+        private SpriteBatch _spriteBatch;
+
+        private float highestY = 0f;
+
+
+        //TODO delete this
+        int totalFrames = 0;
+        float distanceX = 0;
+        float distanceY = 0;
+        float distanceZ = 0;
+        float currentY = 0;
+
+
+
+        public SoarKinect( Game game ) : base( game )
+        {
+
+            _spriteBatch = (SpriteBatch)game.Services.GetService(typeof(SpriteBatch));
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+
+
+        public override void Initialize()
+        {
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this._sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (this._sensor != null)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this._sensor.SkeletonStream.Enable();
+
+                //we only care about the arms and above
+                this._sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+
+                // Add an event handler to be called whenever there is new color frame data
+                this._sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                // Start the sensor!
+                try
+                {
+                    this._sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this._sensor = null;
+                }
+            }
+
+            if (this._sensor == null)
+            {
+                //nothing to do
+            }
+
+            base.Initialize();
+        }
+
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.DrawString(Resources.Instance.GetFont("Cooper"), "X: " + distanceX.ToString(), new Vector2(50, 200), Color.White);
+            _spriteBatch.DrawString(Resources.Instance.GetFont("Cooper"), "Y: " + distanceY.ToString(), new Vector2(50, 250), Color.White);
+            _spriteBatch.DrawString(Resources.Instance.GetFont("Cooper"), "Z: " + distanceZ.ToString(), new Vector2(50, 300), Color.White);
+            _spriteBatch.DrawString(Resources.Instance.GetFont("Cooper"), "c: " + currentY.ToString(), new Vector2(50, 350), Color.White);
+            base.Draw(gameTime);
+        }
+
+
+        /**
+         * 
+         */
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    ++totalFrames;
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+
+                if (skeletons.Length != 0 && skeletons[0] != null)
+                {
+                    foreach (Skeleton skel in skeletons)
+                    {
+                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            SkeletonPoint left = skel.Joints[JointType.WristLeft].Position;
+                            SkeletonPoint right = skel.Joints[JointType.WristRight].Position;
+                            distanceY = left.Y - right.Y;
+                            distanceZ = left.Z - right.Z;
+                            distanceX = left.X - right.X;
+                            if (Math.Abs(left.Y - right.Y) < 0.2f)
+                            {
+                                if( Math.Abs( left.Y - currentY ) > 0.05f )
+                                    Resources.Instance.MoveBirdUp();
+                                
+                                currentY = left.Y;
+                            }
+                            else if ((left.Y - right.Y) < -0.5f)
+                            {
+                                Resources.Instance.MoveBirdLeft();
+                            }
+                            else if ((left.Y - right.Y) > 0.5f)
+                            {
+                                Resources.Instance.MoveBirdRight();
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
+
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this._sensor)
+            {
+                this._sensor.Stop();
+            }
+        }
+
+    }
+
+}
