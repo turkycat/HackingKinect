7ff56ac03fe508bb4d8b1ed35d6ad863422287f4
Karl Akert <kpakert@gmail.com>
2014-02-08 09:28:33 -0600

    Improved collisions greatly between birds

--- KinectoSoar/KinectoSoar/SpriteManager/BlackBird.cs
+++ KinectoSoar/KinectoSoar/SpriteManager/BlackBird.cs
@@ -26,8 +26,12 @@ namespace KinectoSoar.SpriteManager
         private int _waitTime;
         private bool _wait;
 
+        private Rectangle tempBird;
+
         public BlackBird(Game game, SpriteBatch spriteBatch) : base( game, spriteBatch )
         {
+            tempBird = new Rectangle(0, 0, 0, 0);
+
             this._birdInfo = new List<SpriteReader.SpriteInfo>();
             _birdInfo.Add(Resources.Instance.GetSpriteBlackInfo("Bird1"));
             _birdInfo.Add(Resources.Instance.GetSpriteBlackInfo("Bird2"));
@@ -45,8 +49,9 @@ namespace KinectoSoar.SpriteManager
         
         public override bool IsColliding(Sprite sprite)
         {
-            Rectangle thisBird = new Rectangle((int)Position.X - WIDTH / 4, (int)Position.Y - HEIGHT / 2 + 20, WIDTH / 2, HEIGHT - 80);
-            Rectangle bird = new Rectangle((int)(sprite.Position.X - 50), (int)(sprite.Position.Y - 50 + 13 ), 100, 50);
+            Rectangle thisBird = new Rectangle((int)Position.X - WIDTH / 4, (int)Position.Y - HEIGHT / 2 + 55, WIDTH / 2, HEIGHT - 110);
+            Rectangle bird = new Rectangle((int)(sprite.Position.X - 50), (int)(sprite.Position.Y - 15 ), 100, 30);
+            tempBird = bird;
             if (thisBird.Intersects(bird))
                 return true;
             return false;
@@ -96,6 +101,8 @@ namespace KinectoSoar.SpriteManager
             Rectangle dest = new Rectangle((int)Position.X - WIDTH / 2, (int)Position.Y - HEIGHT / 2, WIDTH, HEIGHT);
             Rectangle source = _birdInfo[_frameIndex].Position;
             base._spriteBatch.Draw(Resources.Instance.GetTexture( "BlackBirdSprite" ), dest, source, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.1f);
+            DrawRect(new Rectangle((int)Position.X - WIDTH / 4, (int)Position.Y - HEIGHT / 2 + 55, WIDTH / 2, HEIGHT - 110));
+            DrawRect(tempBird);
         }
 
         private void SetRandomPosition()
@@ -105,6 +112,15 @@ namespace KinectoSoar.SpriteManager
             _waitTime = Resources.Instance.Rand.Next(1000, 6000);
             Position = new Vector2(x, -1 * HEIGHT);
             _wait = true;
+        }
+
+        private void DrawRect(Rectangle rect)
+        {
+            Texture2D line = Resources.Instance.GetTexture("LineTexture");
+            _spriteBatch.Draw(line, new Rectangle(rect.X, rect.Y, rect.Width, 5), Color.White); // top line
+            _spriteBatch.Draw(line, new Rectangle(rect.X, rect.Y, 5, rect.Height), Color.White); // left line
+            _spriteBatch.Draw(line, new Rectangle(rect.X, rect.Height - 5 + rect.Y, rect.Width, 5), Color.White); // bottom line
+            _spriteBatch.Draw(line, new Rectangle(rect.Width - 5 + rect.X, rect.Y, 5, rect.Height), Color.White); // right line
         }
     }
 }
